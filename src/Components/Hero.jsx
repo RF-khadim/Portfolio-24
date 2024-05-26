f64ba1b46ad8c4926be28e4f02c47cce9ddb0933
rf-khadim <khadimarif52@gmail.com>
2024-05-26 07:27:26 -0700

    chore: removed width

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <div id='hero' className='w-full h-auto md:h-full  bg-gradient-to-r from-[#202020] to-[#34444c]'>
             <div className='md:mt-10 w-full h-screen flex md:justify-around  items-center px-10 md:px-20 gap-4 flex-col md:flex-row '>
 
-                <div className='w-[500px] md:w-[600px] h-auto flex items-center justify-center flex-col' >
+                <div className='h-auto flex items-center justify-center flex-col' >
                     <div className='flex flex-col text-white md:text-2xl sm:text-xl  gap-3 font-bold text-center mt-28'>
                         <span >Hi it's Me </span>
                         <h1 className='text-gradient md:text-5xl sm:text-3xl'>Muhammad Arif</h1>
